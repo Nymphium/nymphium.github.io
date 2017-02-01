a676b8fb4f4970ab9fa25fc606f8c72445b040af
nymphium <s1311350@coins.tsukuba.ac.jp>
2017-02-01 13:11:02 +0900

    deploy at 2017-02-01 13:11:02 +0900

--- _plugins/fourty_description.rb
+++ _plugins/fourty_description.rb
@@ -1,14 +1,14 @@
-require'nokogiri'
-require'cgi'
+# require'nokogiri'
+# require'cgi'
 
-def desc my_html
-	CGI.escapeHTML(Nokogiri::HTML(my_html.strip.gsub(/\R/, "").gsub(/^.*<divclass="post-content"itemprop="articleBody">/, "")).text.slice(1, 34)) + "......"
-end
+# def desc my_html
+	# CGI.escapeHTML(Nokogiri::HTML(my_html.strip.gsub(/\R/, "").gsub(/^.*<divclass="post-content"itemprop="articleBody">/, "")).text.slice(1, 34)) + "......"
+# end
 
-Jekyll::Hooks.register :site, :pre_render do |post, cont|
-	cont.site.posts.each{|hoge|
-		description = desc(hoge.content.inspect)
-		hoge.data["description"] = description.strip.gsub(/\R/, "")
-	}
-end
+# Jekyll::Hooks.register :site, :pre_render do |post, cont|
+	# cont.site.posts.each{|hoge|
+		# description = desc(hoge.content.inspect)
+		# hoge.data["description"] = description.strip.gsub(/\R/, "")
+	# }
+# end
 
