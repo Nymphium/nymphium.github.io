e778baf414b4f430cc302be0c34e12fc49de79f5
Kawahara Satoru <s1311350@coins.tsukuba.ac.jp>
2017-11-16 00:16:46 +0900

    お前それキャッシュの意味なかったやんけ…

--- _plugins/ordered/md_filters/99-lazy_tweet_embedding.rb
+++ _plugins/ordered/md_filters/99-lazy_tweet_embedding.rb
@@ -7,14 +7,14 @@ lambda{|content|
 	embedded_content = content
 	content.scan(/(https?:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/([0-9]+)\/?)/).each do |url, id|
 		begin
-			tweet_json = open("https://api.twitter.com/1/statuses/oembed.json?id=#{id}").read
 			tweet_html = nil
 
 			if File.exist? "twicache/#{id}" then
 				File.open("twicache/#{id}", "r"){|f| tweet_html = f.read }
 			else
+				tweet_json = open("https://api.twitter.com/1/statuses/oembed.json?id=#{id}").read
 				tweet_html = JSON.parse(tweet_json, { :symbolize_names => true })[:html]
-				File.open("twicache/#{id}", "w").write(tweet_html).close
+				File.open("twicache/#{id}", "w"){|f| f.write(tweet_html)}
 			end
 
 			embedded_content = embedded_content.gsub(/#{url}/, "<div class=\"enclosed-tweet\" align=\"center\">#{tweet_html}</div>")
