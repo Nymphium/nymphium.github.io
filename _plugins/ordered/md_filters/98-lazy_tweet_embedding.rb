5b569e249fb66fe225d44c3e836a550e330c8277
Nymphium <s1311350@gmail.com>
2021-07-28 01:58:06 +0900

    deploy at 2021-07-28 01:58:06 +0900

--- _plugins/ordered/md_filters/98-lazy_tweet_embedding.rb
+++ _plugins/ordered/md_filters/98-lazy_tweet_embedding.rb
@@ -1,27 +1,27 @@
 # https://github.com/takuti/jekyll-lazy-tweet-embedding
-require "open-uri"
-require "json"
+require 'open-uri'
+require 'json'
 
 # convert tweet url to embedding html
-lambda{|content|
-	embedded_content = content
-	content.scan(/(https?:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/([0-9]+)\/?)/).each do |url, id|
-		begin
-			tweet_html = nil
+lambda { |content|
+  embedded_content = content
+  content.scan(%r{(https?://twitter\.com/[a-zA-Z0-9_]+/status/([0-9]+)/?)}).each do |url, id|
+    tweet_html = nil
 
-			if File.exist? "twicache/#{id}" then
-				File.open("twicache/#{id}", "r"){|f| tweet_html = f.read }
-			else
-				tweet_json = open("https://api.twitter.com/1/statuses/oembed.json?id=#{id}").read
-				tweet_html = JSON.parse(tweet_json, { :symbolize_names => true })[:html]
-				File.open("twicache/#{id}", "w"){|f| f.write(tweet_html)}
-			end
+    if File.exist? "twicache/#{id}"
+      File.open("twicache/#{id}", 'r') { |f| tweet_html = f.read }
+    else
+      tweet_json = URI.open("https://api.twitter.com/1/statuses/oembed.json?id=#{id}").read
+      tweet_html = JSON.parse(tweet_json, { symbolize_names: true })[:html]
+      File.open("twicache/#{id}", 'w') { |f| f.write(tweet_html) }
+    end
 
-			embedded_content = embedded_content.gsub(/#{url}/, "<div class=\"enclosed-tweet\" align=\"center\">#{tweet_html}</div>")
-		rescue
-			embedded_content = embedded_content.gsub(/#{url}/, "<span class=\"twitter-not-found\">this tweet not found: #{url}</span>")
-		end
-	end
+    embedded_content = embedded_content.gsub(/#{url}/,
+                                             "<div class=\"enclosed-tweet\" align=\"center\">#{tweet_html}</div>")
+  rescue OpenURI::HTTPError
+    embedded_content = embedded_content.gsub(/#{url}/,
+                                             "<span class=\"twitter-not-found\">this tweet not found: #{url}</span>")
+  end
 
-	embedded_content
+  embedded_content
 }
