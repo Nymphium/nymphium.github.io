cceacf000f0551e6ceb43fa2cc972ec723fd878d
Nymphium <s1311350@gmail.com>
2023-04-17 21:16:51 +0900

    update

--- shell.nix
+++ shell.nix
@@ -11,8 +11,9 @@ mkShell {
     ruby
     solargraph
   ];
+  LOCALE_ARCHIVE = lib.optionalString stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
+  LANG="en_US.UTF-8";
   shellHook = ''
-    export LANG="en_US.UTF-8"
     export PATH="''${PWD}/.bundle/ruby/${ruby.version.libDir}/bin":''${PATH}
     bundle config set --local path "''${PWD}/.bundle"
   '';
